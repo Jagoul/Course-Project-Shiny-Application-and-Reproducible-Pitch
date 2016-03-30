d43926077df932bfe161963decc3d656f9e13da9
Victor Yeh <chihkaiyeh@gmail.com>
2016-03-31 00:09:53 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,7 +1,6 @@
 library(shiny)
 shinyUI(
   pageWithSidebar(
-    # Application title
     headerPanel("Golden Cup Coffee Ratio"),
     sidebarPanel(
       numericInput('golden', 'Cups You Would Like To Brew', 1, min = 1, max = 12, step = 1
